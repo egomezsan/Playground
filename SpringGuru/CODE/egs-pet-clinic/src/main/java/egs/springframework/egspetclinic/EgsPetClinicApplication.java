3a5f98031a061be266e82ac11c6898b4276c92a9
Eduardo Gomez <unagi45@hotmail.com>
2019-01-09 00:45:04 -0600

    Add initial project to repo

---
+++ SpringGuru/CODE/egs-pet-clinic/src/main/java/egs/springframework/egspetclinic/EgsPetClinicApplication.java
@@ -0,0 +1,14 @@
+package egs.springframework.egspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgsPetClinicApplication.class, args);
+    }
+
+}
+
